e02599ee56ec65b508884b42723e39655acf9423
TheChilliPL <dragonacchilli@gmail.com>
2020-10-17 14:02:06 +0200

    Start voting module refactoring

--- src/main/kotlin/me/patrykanuszczyk/traitorbot/modules/voting/Voting.kt
+++ src/main/kotlin/me/patrykanuszczyk/traitorbot/modules/voting/Voting.kt
@@ -3,7 +3,7 @@ package me.patrykanuszczyk.traitorbot.modules.voting
 import net.dv8tion.jda.api.entities.Message
 
 class Voting (
-    val name: String?,
+    val name: String,
     val title: String,
     val answers: List<String>,
     val message: Message
--- src/main/kotlin/me/patrykanuszczyk/traitorbot/modules/voting/VotingModule.kt
+++ src/main/kotlin/me/patrykanuszczyk/traitorbot/modules/voting/VotingModule.kt
@@ -1,5 +1,63 @@
-//package me.patrykanuszczyk.traitorbot.modules.voting
-//
+package me.patrykanuszczyk.traitorbot.modules.voting
+
+import me.patrykanuszczyk.traitorbot.TraitorBot
+import me.patrykanuszczyk.traitorbot.commands.BranchCommand
+import me.patrykanuszczyk.traitorbot.commands.Command
+import me.patrykanuszczyk.traitorbot.commands.Parameter
+import me.patrykanuszczyk.traitorbot.commands.arguments.CommandInvokeArguments
+import me.patrykanuszczyk.traitorbot.commands.arguments.DiscordCommandInvokeArguments
+import me.patrykanuszczyk.traitorbot.commands.parseParameters
+import me.patrykanuszczyk.traitorbot.modules.BotModule
+import me.patrykanuszczyk.traitorbot.utils.alphanumerics
+import me.patrykanuszczyk.traitorbot.utils.getRandomString
+import org.jetbrains.annotations.Contract
+
+class VotingModule(bot: TraitorBot) : BotModule(bot) {
+
+    val newCommand = Command("new") {
+        if(it !is DiscordCommandInvokeArguments || !it.isFromGuild) {
+            return@Command it.reply("Tej komendy można użyć tylko na serwerze!")
+        }
+
+        var name: String? = null
+        val parameters = try {
+            parseParameters(
+                it.parameters,
+                Parameter("n", "name", getInput = true) { name = it }//,
+//                Parameter("t", "timeout", getInput = true) { throw UnsupportedOperationException() },
+//                Parameter("a", "anonymous") { throw UnsupportedOperationException() },
+//                Parameter("d", "no-duplicates") { throw UnsupportedOperationException() },
+//                Parameter("m", "min-votes", getInput = true) { throw UnsupportedOperationException() },
+//                Parameter("w", "show-winners") { throw UnsupportedOperationException() },
+//                Parameter("W", "unique-winner") { throw UnsupportedOperationException() },
+//                Parameter("r", "needs-role") { throw UnsupportedOperationException() },
+//                Parameter("no-removing") { throw UnsupportedOperationException() }
+            )
+        } catch (e: UnsupportedOperationException) {
+            return@Command it.reply("Jeden z wykorzystanych parametrów nie jest obecnie obsługiwany.")
+        }
+
+        // Ensure guild voting map exists
+        votings.computeIfAbsent(it.guild!!.idLong) { mutableMapOf() }
+
+        // Random name generating
+        if(name == null) {
+            do {
+                name = getRandomString(alphanumerics, 10)
+            } while(name in votings[it.guild!!.idLong]!!)
+        }
+
+        //val voting = Voting(name!!, )
+    }.withAliases("create", "add")
+
+    val rootCommand = BranchCommand(
+        "voting",
+        newCommand
+    ).andRegister(bot)
+
+    var votings = mutableMapOf<Long, MutableMap<String, Voting>>()
+    val voteEmoji = listOf("1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣")
+}
 //import me.patrykanuszczyk.traitorbot.TraitorBot
 //import me.patrykanuszczyk.traitorbot.commands.*
 //import me.patrykanuszczyk.traitorbot.commands.arguments.CommandInvokeArguments
